d7fbf816214ca467b2223f399e4d4df7898a33da
pulkitmadaan <pulkitmadaan1997@gmail.com>
2020-12-01 18:55:50 +0530

    nerw change

--- AntioneIndustries/settings.py
+++ AntioneIndustries/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
